ce5743a9e3754de6d93cc0e99de5bc882bc0a3af
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2025-06-15 19:28:01 +0500

    Edit: install script

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,19 +1,16 @@
-sudo cp evdev.* /usr/share/X11/xkb/rules \
-  && mkdir -p ~/.xkb/symbols \
+mkdir -p ~/.xkb/symbols \
   && cp orpw ~/.xkb/symbols/ \
   && cp orn ~/.xkb/symbols/ \
   && cp orp ~/.xkb/symbols/ \
   && cp orw ~/.xkb/symbols/ \
-  && echo "
-    xkb_keymap {
-      xkb_keycodes  { include "evdev+aliases(qwerty)" };
-      xkb_types     { include "complete" };
-      xkb_compat    { include "complete" };
-      xkb_symbols   {
-        include "pc+us"
-        include "orpw(ornate-pw)" // 'orpw' is your layout file
-      };
-      xkb_geometry  { include "pc(pc104)" };
-    };
-    " >> ~/.xkb/keymap \
+  && echo 'xkb_keymap {
+  xkb_keycodes  { include "evdev+aliases(qwerty)" };
+  xkb_types     { include "complete" };
+  xkb_compat    { include "complete" };
+  xkb_symbols   {
+    include "pc+us"
+    include "orpw(ornate-pw)" // 'orpw' is your layout file
+  };
+  xkb_geometry  { include "pc(pc104)" };
+};' >> ~/.xkb/keymap \
     && xkbcomp -I$HOME/.xkb ~/.xkb/keymap $DISPLAY
